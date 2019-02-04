3c8539fdd015740ad08e5d5fd20a1b9a0a8f21ce
matharman <mlharma@g.clemson.edu>
2019-02-04 17:06:37 -0500

    Added main skeleton

--- openCV/main.c
+++ openCV/main.c
@@ -1,39 +1,45 @@
 #include <stdio.h>
 #include <stdlib.h>
-
-//#include <highgui.h>
+#include <pthreads.h>
 
 #include "include/cam.h"
-#include "include/track.h"
 
 typedef struct _vid_cap_data {
-    CLEyeCameraInstance cam;
-    PBYTE frame;
-} VideoCapData;
-
-typedef struct _img_proc_data {
-    IplImage *img;
-    int avg_fps;
-} ImgProcData;
+    CvCapture *cam;
+    CvMat frame;
+} video_cap_data_t;
 
 /* Video capture thread */
-static DWORD WINAPI VideoCapThread(LPVOID user_data) {
-    VideoCapData *params = (VideoCapData *)user_data[0];
-    VideoCapData *params = (VideoCapData *)user_data[0];
+void *video_cap_thread(void *userdata) {
+    video_cap_data_t *params = (video_cap_data_t *)userdata;
+
+    size_t frame_rows = cam_frame_r(params->cam);
+    size_t frame_cols = cam_frame_c(params->cam);
+
+    CvMat *frame = cvCreateMat(frame_rows, frame_cols, CV_8UC1);
+
+    namedWindow("Debug Stream", WINDOW_AUTOSIZE);
 
     while(1) {
-        /* Capture Frame */
-        CLEyeCameraGetFrame(params->cam,params->frame);
+        if(cam_capture_frame(params->cam, frame) != EXIT_SUCCESS) {
+            fprintf(stderr, "Frame capture failed!\n");
+            return NULL;
+        }
+
+        imshow("Debug Stream", frame);
     }
 }
 
-int _tmain(int argc, _TCHAR* argv[]) {
+int main(int argc, char* argv[]) {
 
     int err = EXIT_SUCCESS;
 
+    (void)argc;
+    (void)argv;
+
     /* Video capture parameters */
-    HANDLE cap_thread;
-    VideoCapData cap_params = {
+    pthread_t cap_thread;
+    video_cap_data_t cap_params = {
         .cam = NULL,
         .frame = NULL
     };
@@ -44,12 +50,14 @@ int _tmain(int argc, _TCHAR* argv[]) {
         goto exit;
     }
 
-    cap_thread = CreateThread(NULL, 0, &VideoCapThread, &cap_params, 0, 0);
+    cap_thread = pthread_create(&cap_thread, NULL, &video_cap_thread, &cap_params);
     if(!cap_thread) {
         fprintf(stderr, "Failed to create Capture Thread!\n");
         err = EXIT_FAILURE;
         goto exit;
     }
+
+    pthread_join(&cap_thread, NULL);
 
 exit:
 
