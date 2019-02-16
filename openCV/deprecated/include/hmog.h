b0f12090f6b4b4a308139705da582a3978cbece4
matharman <mlharma@g.clemson.edu>
2019-02-15 22:12:47 -0500

    Removed deprecation folder as master is now tracking it

--- openCV/deprecated/include/hmog.h
+++
@@ -1,8 +0,0 @@
-#ifndef HMOG_H
-#define HMOH_H
-
-#include <opencv/cv.h>
-#include <opencv/cxcore.h>
-#include <opencv/highgui.h>
-
-#endif
--- openCV/deprecated/track.c
+++
@@ -1,7 +0,0 @@
-#include <stdio.h>
-#include <stdlib.h>
-
-#include "include/cam.h"
-#include "include/track.h"
-
-
