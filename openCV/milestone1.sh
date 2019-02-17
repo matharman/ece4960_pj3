46541e15039f3efc3e1b681662d0c3f315fac847
matharman <mlharma@g.clemson.edu>
2019-02-17 15:37:14 -0500

    Improved plotting in milestone demo script

--- openCV/milestone1.sh
+++ openCV/milestone1.sh
@@ -17,17 +17,16 @@ fi
 
 gnuplot -p <<-EOFMARKER
     set datafile separator ","
-    set multiplot layout 1,2 title "Centroid Motion Data"
-    
-    #
+
+    set term x11 0
     set title "Centroid Position"
-    unset key
+    set xlabel "Time (20ms)"
+    set ylabel "Position (pixels)"
     plot for [col=1:2] 'circle_data.txt' using col with lines title columnheader
-    
-    #
+
+    set term x11 1
     set title "Centroid Velocity"
-    unset key
-    plot 'circle_data.txt' using 3 with lines columnheader
-    
-    unset multiplot
+    set xlabel "Time (20ms)"
+    set ylabel "Velocity (pixels / sec)"
+    plot '' using 3 with lines title columnheader
 EOFMARKER
